c33919135f68102ca4a3feb8e8fc877fcd8abd85
Vidur Pandiripally <vkpandiripally@gmail.com>
2023-10-26 16:15:02 -0500

    "Edited the phppractice to output table"

--- phppractice4.php
+++ phppractice4.php
@@ -2,21 +2,47 @@
 <html>
 <head>
     <title>Multiplication Table Generator</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <h2>Multiplication Table for 6</h2>
-    <ul>
     <?php
-    // Number for the multiplication table
-    $number = $_POST["number"];
-    
-    // Generate the multiplication table
-    for ($i = 1; $i <= 10; $i++) {
-        $result = $number * $i;
-        echo "<li>$number x $i = $result</li>";
+    // Check if the form is submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the user input
+        $number = $_POST["number"];
+
+        // Validate the input (check if it's a positive integer)
+        if (ctype_digit($number) && $number > 0) {
+    ?>
+            <table>
+                <?php
+                // Generate and print rows for the multiplication table
+                for ($row = 1; $row <= $number; $row++) {
+                    echo "<tr><th>$row</th>";
+                    for ($col = 1; $col <= $number; $col++) {
+                        $result = $row * $col;
+                        echo "<td>$result</td>";
+                    }
+                    echo "</tr>";
+                }
+                ?>
+            </table>
+    <?php
+        }
     }
     ?>
-    </ul>
 </body>
 </html>
+
+
 
